66b9d4903e99eae84dae0a8521e4127f70f5ee57
pranav-weetech <pranav@weetechsolution.com>
2025-11-26 15:25:12 +0530

    push main

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ const App = () => {
         <header className="app-header">
           <div className="app-logo">📋</div>
           <h1 className="app-title">
-            {isDev ? "main-1 TaskFlow Pro" : "TaskFlow Pro"}
+            {isDev ? "main- TaskFlow Pro" : "TaskFlow Pro"}
           </h1>
           {isDev && (
             <span
